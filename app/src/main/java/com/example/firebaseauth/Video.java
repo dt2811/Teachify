db87983d1fd261bd0721f0a2ee532a00e3808547
himanshuharlalka <himanshuharlalka@gmail.com>
2020-09-26 16:03:53 +0530

    login page, video act

---
+++ app/src/main/java/com/example/firebaseauth/Video.java
@@ -0,0 +1,29 @@
+package com.example.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Video extends AppCompatActivity {
+    VideoView videoView;
+   //url
+    private String url="url";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        videoView=findViewById(R.id.video_view);
+
+        MediaController mediaController=new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri=Uri.parse(url);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+    }
+}
